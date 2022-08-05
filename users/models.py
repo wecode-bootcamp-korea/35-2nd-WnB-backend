e9b1fbe5e70d61ce8db233df9d114cc727f1d66c
eunhyung <ooooo5h91@gmail.com>
2022-08-05 15:19:41 +0900

    모델링 수정

--- users/models.py
+++ users/models.py
@@ -4,13 +4,13 @@ from django.db      import models
 from core.models    import TimeStampModel
 
 class User(TimeStampModel):
-    first_name          = models.CharField(max_length = 50)
-    last_name           = models.CharField(max_length = 50)
+    first_name          = models.CharField(max_length = 50, null=True)
+    last_name           = models.CharField(max_length = 50, null=True)
     email               = models.CharField(max_length = 50)
     kakao_id            = models.BigIntegerField(unique=True)
     kakao_profile_img   = models.CharField(max_length = 500)
-    phone_number        = models.CharField(max_length = 50)
-    birth_day           = models.CharField(max_length = 50)
+    phone_number        = models.CharField(max_length = 50, null=True)
+    birth_day           = models.CharField(max_length = 50, null=True)
     point               = models.DecimalField(max_digits = 10, decimal_places = 2, default=Decimal('100000.00'))
         
     class Meta:
