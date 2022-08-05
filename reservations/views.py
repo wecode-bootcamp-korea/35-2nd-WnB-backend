79f6b916891c11e355517fc2d7d61f8720c4b96c
apple <apple@apples-MacBook-Pro.local>
2022-08-05 16:59:24 +0900

    예약API  GET 구현 & tests.py작성

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http            import JsonResponse
+from django.views           import View
 
-# Create your views here.
+from reservations.models    import Reservation
+from core.utils             import signin_decorator
+
+class ResevationsView(View):
+    @signin_decorator
+    def get(self, request): 
+        user            = request.user
+        reservations    = Reservation.objects.select_related("room") \
+                                             .prefetch_related("room__image_set") \
+                                             .filter(user = user)
+        result = [{
+            'reservation_number'        : reservation.number,
+            'user_name'                 : reservation.user.last_name + " " + reservation.user.first_name,
+            'room'                      : reservation.room.name,
+            'price'                     : reservation.price,
+            'people'                    : reservation.people,
+            'check_in'                  : reservation.check_in,
+            'check_out'                 : reservation.check_out,
+            'img'                       : reservation.room.image_set.all()[0].url
+        } for reservation in reservations]
+
+        return JsonResponse({"RESULT": result}, status=200)
