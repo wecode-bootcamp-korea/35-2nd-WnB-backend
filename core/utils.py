5e1c86eabfc7bc02e6fcdf8f686d99ccc51f724d
eunhyung <ooooo5h91@gmail.com>
2022-08-08 17:08:37 +0900

    유효성 검사 코드 작성

--- core/utils.py
+++ core/utils.py
@@ -1,8 +1,10 @@
-import jwt
+import jwt    
+import re
 
-from django.http            import JsonResponse
 from django.conf            import settings
-    
+from django.core.exceptions import ValidationError
+from django.http            import JsonResponse
+
 from users.models           import User
 
 def signin_decorator(func):
@@ -20,3 +22,15 @@ def signin_decorator(func):
         except jwt.exceptions.DecodeError:
             return JsonResponse({'message':'INVALID_TOKEN'}, status=400)
     return wrapper
+
+def check_first_name(value):
+    if not re.match('^[ㄱ-ㅎ|가-힣]+$', value):
+        raise ValidationError("FIRST_NAME_ERROR")
+    
+def check_last_name(value):
+    if not re.match('^[ㄱ-ㅎ|가-힣]+$', value):
+        raise ValidationError("LAST_NAME_ERROR")
+    
+def check_phone_number(value):
+    if not re.match('^01([0|1|6|7|8|9])-([0-9]{3,4})-([0-9]{4})$', value):
+        raise ValidationError("PHONE_NUMBER_ERROR")
